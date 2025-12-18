b64ef240061c1d60ea5c5e88855df5933bf43639
@compulim <@compulim>
2025-12-18 05:53:52 +0000

    Bump scaffold

--- packages/atelier/tsup.config.ts
+++ packages/atelier/tsup.config.ts
@@ -1,23 +1,13 @@
-import { defineConfig, type Options } from 'tsup';
-
-const COMMON_CONFIG: Options = {
-  define: {
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || ''),
-    'process.env.npm_package_version': JSON.stringify(process.env.npm_package_version || '0.0.0-0')
-  },
-  dts: true,
-  sourcemap: true,
-  target: 'esnext',
-
-  // TODO: Remove below when fully migrated.
-  // dts: false,
-  platform: 'browser'
-};
+import { defineConfig } from 'tsup';
 
 export default defineConfig([
   {
-    ...COMMON_CONFIG,
-    entry: { 'atelier': './src/index.ts' },
-    format: ['cjs', 'esm']
-  },
+    dts: true,
+    entry: {
+      ['atelier'.split('/').at(-1) as string]: './src/index.ts'
+    },
+    format: ['cjs', 'esm'],
+    sourcemap: true,
+    target: 'esnext'
+  }
 ]);
